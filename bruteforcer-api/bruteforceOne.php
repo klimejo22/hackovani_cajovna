740812d2949d018576b2b40a44254881021ce9cc
klimejo22 <klimejo22@sps-prosek.cz>
2026-01-06 10:36:47 +0100

    fix

--- bruteforcer-api/bruteforceOne.php
+++ bruteforcer-api/bruteforceOne.php
@@ -1,7 +1,7 @@
 <?php
 require_once "lib/bruteforce.php";
 
-header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 
